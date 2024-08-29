0f2c1a18022bb53e7941e1f77e00aa808e457f68
81392-13243423 <81392-13243423@CLAB3-01>
2024-08-29 12:56:25 +0800

    Add project files.

---
+++ EnrollmentSystem/Models/Entities/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentSystem.Models.Entities
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [MaxLength(25)]
+        public string ?Firstname { get; set; }
+        [MaxLength(25)]
+        public string Lastname { get; set; } = string.Empty;
+    }
+}
