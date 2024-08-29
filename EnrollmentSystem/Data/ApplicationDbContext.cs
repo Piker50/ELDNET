c8058f4a92c9425065fdcc64b8952b106e24d933
Piker50 <lanzzboy@gmail.com>
2024-08-29 13:34:34 +0800

    Update

---
+++ EnrollmentSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EnrollmentSystem.Models.Entities;
+
+namespace EnrollmentSystem.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
